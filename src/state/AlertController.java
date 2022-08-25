1201d2a14c404142a43a521bd4bf664894115bed
miracle.choo <chunam76@gmail.com>
2022-08-25 09:22:00 +0900

    과제 소스 업데이트

---
+++ src/state/AlertController.java
@@ -0,0 +1,21 @@
+package state;
+
+public class AlertController {
+	private AlertState state;
+	
+	public void setState( AlertState state) {
+		
+	}
+	
+	
+	public void alert() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void switchMode() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
